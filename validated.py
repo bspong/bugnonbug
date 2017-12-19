e53d8efe554ab794a714c52152d9e768896aa618
bspong <boonchoo.sri@msu.ac.th>
2017-12-19 23:10:43 +0700

    Delet Dup Folder

--- validated.py
+++ validated.py
@@ -1,1 +1,7 @@
-from sklearn.model_selection import cross_val_score
+#from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+
+
+kf = KFold(n_splits=2)
+for train, test in kf.split(X):
+    print ()
